096332d19205dc8b92fcdfc43b5fb997ca5aae00
Nestan <nestankeneshova16@gmail.com>
2023-02-28 14:18:32 +0600

    changed element styles

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ let sayHelloBtn = document.querySelector("say-hello");
 
 //изменить свойства элемента (сод-е, класс, итд)
 sayHelloBtn.textContent = "SAY HELLO";
+sayHelloBtn.className = "btn btn-red";//соз класс к кнопке 
+sayHelloBtn.classList.add("btn", "btn-red")//соз класс
+
+//изменить стили элемента
+sayHelloBtn.style.color = "tomato";
+sayHelloBtn.style. fontSize = "20px";
