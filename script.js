0d36fcc3bdea856150f522ba9f3372a88087d4a0
Nestan <nestankeneshova16@gmail.com>
2023-02-28 14:13:17 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+//найти элемент используя css селектор
+let sayHelloBtn = document.querySelector("say-hello");
+
+//изменить свойства элемента (сод-е, класс, итд)
+sayHelloBtn.textContent = "SAY HELLO";
